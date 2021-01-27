2a853639cec19d20fdeded6229448a40135eeadb
danielrulez@gmail.com <danielrulez@gmail.com>
2021-01-27 12:30:55 +0000

    Updated for kenshoo

--- test.scala
+++ test.scala
@@ -56,3 +56,31 @@ print("Hello from databricks2")
 
 // COMMAND ----------
 
+// MAGIC %python
+// MAGIC spark.sqlContext.conf.set("spark.redis.host", "server1")
+// MAGIC loadedDf = spark.read.option("spark.redis.host", "server1").format("org.apache.spark.sql.redis").option("table", "person").load()
+
+// COMMAND ----------
+
+// MAGIC %python
+// MAGIC from pyspark.sql import SparkSession
+// MAGIC 
+// MAGIC spark2 = SparkSession.builder.config("spark.redis.host", "server2").config("spark.redis.port", "6379").getOrCreate();
+// MAGIC             
+// MAGIC spark.conf.set("spark.redis.host", "server1")
+// MAGIC spark._jsc.getConf().set("spark.redis.host", "server1")
+// MAGIC spark.sparkContext.getConf().set("spark.redis.host", "server1")
+// MAGIC loadedDf = spark2.read.option("spark.redis.host", "server1").format("org.apache.spark.sql.redis").option("table", "person").load()
+
+// COMMAND ----------
+
+// MAGIC %python
+// MAGIC from pyspark.sql import SparkSession
+// MAGIC 
+// MAGIC spark2 = SparkSession.builder.appName("myApp").config("spark.redis.host", "server9").config("spark.redis.port", "6379").config("spark.redis.auth", "passwd").getOrCreate()
+// MAGIC 
+// MAGIC 
+// MAGIC loadedDf = spark2.read.option("spark.redis.host", "server1").format("org.apache.spark.sql.redis").option("table", "person").load()
+
+// COMMAND ----------
+
