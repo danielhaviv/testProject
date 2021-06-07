dd9b60d074f3f7ce44152076818e1418b0060829
Daniel Haviv <danielrulez@gmail.com>
2021-06-07 12:23:21 +0300

    Update testWidget.py

--- testWidget.py
+++ testWidget.py
@@ -3,7 +3,7 @@ dbutils.widgets.dropdown("X123", "1", [str(x) for x in range(1, 10)])
 
 # COMMAND ----------
 
-print("hello value of widget is:", dbutils.widgets.get("X123"))
+print("hello value is:", dbutils.widgets.get("X123"))
 
 # COMMAND ----------
 
