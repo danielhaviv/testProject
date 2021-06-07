b8eae9beec6de32034af611c3d862af1f44aeeb8
danielrulez@gmail.com <danielrulez@gmail.com>
2021-06-07 08:36:36 +0000

    committing a widget

--- testWidget.py
+++ testWidget.py
@@ -3,7 +3,7 @@ dbutils.widgets.dropdown("X123", "1", [str(x) for x in range(1, 10)])
 
 # COMMAND ----------
 
-print("hello", dbutils.widgets.get("X123"))
+print("hello value is:", dbutils.widgets.get("X123"))
 
 # COMMAND ----------
 
