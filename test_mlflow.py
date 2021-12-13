f83afeb8e53a70cf4efcd2dc31ff0ccee346528e
danielrulez@gmail.com <danielrulez@gmail.com>
2021-12-13 09:40:31 +0000

    another mlflow

--- test_mlflow.py
+++ test_mlflow.py
@@ -1,8 +1,8 @@
 # Databricks notebook source
 import mlflow
 with mlflow.start_run():
-    mlflow.log_param("x", 1)
-    mlflow.log_metric("y", 2)
+    mlflow.log_param("x", 2)
+    mlflow.log_metric("y", 1)
 
 # COMMAND ----------
 
