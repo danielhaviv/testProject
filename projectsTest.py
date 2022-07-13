309246d3a8cfdfbec327debfcabd17bf8f8b08c9
Daniel Haviv <danielrulez@gmail.com>
2022-07-13 12:29:09 +0300

    Update projectsTest.py

--- projectsTest.py
+++ projectsTest.py
@@ -1,32 +1,5 @@
 # Databricks notebook source
-# MAGIC %pip install function-libs==0.0.1
 
-# COMMAND ----------
-
-print("Goodbye!!")
-
-# COMMAND ----------
-
-print("Hello")
-
-# COMMAND ----------
-
-# Hi Similarweb
-
-# COMMAND ----------
-
-# MAGIC %pip install boto3
-
-# COMMAND ----------
-
-# MAGIC %sql
-# MAGIC select * from table
-
-# COMMAND ----------
-
-# MAGIC %sh ls -ld /mnt/driver-deamon/*
-
-# COMMAND ----------
 
 # COMMAND ----------
 import mlflow
