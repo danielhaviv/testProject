c024e75f188d4b725314a4d358c2eb7763ec1406
Daniel Haviv <danielrulez@gmail.com>
2022-07-13 12:26:22 +0300

    Update projectsTest.py

--- projectsTest.py
+++ projectsTest.py
@@ -28,4 +28,11 @@ print("Hello")
 
 # COMMAND ----------
 
+# COMMAND ----------
+import mlflow
 
+mlflow.set_experiment("/Users/daniel.haviv@databricks.com/my-experiment")
+
+with mlflow.start_run():
+    mlflow.log_param("c", 1)
+    mlflow.log_metric("d", 2)
